3f6bd28c46fbd595f2965c2ac26ea2851757ae4c
Тимофеев Алексей <ATimofeev@lanit.ru>
2019-05-08 15:29:59 +0300

    2

--- src/main/java/restControllers/PersonWithCars.java
+++ src/main/java/restControllers/PersonWithCars.java
@@ -2,6 +2,7 @@ package restControllers;
 
 import database.person.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,9 +13,9 @@ public class PersonWithCars {
     private PersonService personService;
 
 
-    @RequestMapping(value = "/personwithcars?personid={id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/personwithcars?personid={id}", method = RequestMethod.GET ,  produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public model.PersonWithCars getPersonWithCars(@PathVariable final long id) {
+    public model.PersonWithCars getPersonWithCars(@PathVariable long id) {
         return personService.getPersonByID(id);
     }
 
--- src/main/java/restControllers/Statistics.java
+++ src/main/java/restControllers/Statistics.java
@@ -1,13 +1,16 @@
 package restControllers;
 
 import database.auto.service.AutoService;
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.*;
+
 
 @RestController
 public class Statistics {
@@ -15,7 +18,7 @@ public class Statistics {
     @Autowired
     private AutoService autoService;
 
-    @RequestMapping(value = "/statistics" , method = RequestMethod.GET)
+    @RequestMapping(value = "/statistics" , method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
     public model.Statistics savePerson() {
 
         return autoService.getStatistics();
